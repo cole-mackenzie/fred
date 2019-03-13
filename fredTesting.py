ec321fc3b95311b5e0cb4c3a53b6c2c58c900e8f
cole-mackenzie <40571941+cole-mackenzie@users.noreply.github.com>
2019-03-12 22:59:06 -0400

    Add files via upload

--- fredTesting.py
+++ fredTesting.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import time
 import fred.series as fs
+
+seriesIDs = ['UR', 'LF']
+
+# empty Dict
+dict = {}
+
+with open('fredAPIKey.txt', 'r') as f:
+    apiKey = f.read()
+
+abbrevs = pd.read_csv('Abbrevs.csv')
+
+for state in abbrevs.Abbrev:
+    print(state)
+    dict[state] = {}
+    for i in seriesIDs:
+        dict[state][i] = {}
+        id = state + i
+        stateData = fs.fredSeries(apiKey, id)
+        dict[state][i]['Date'] = stateData.xValues
+        dict[state][i]['Value'] = stateData.yValues
+    time.sleep(.5)
